3a49d5eb6fc57a6125b50c30cb667ce170689f0e
seho1278 <seho1278@naver.com>
2024-06-17 22:04:17 +0900

    프로젝트 생성

---
+++ src/main/java/com/springboot/daits/DaitsApplication.java
@@ -0,0 +1,13 @@
+package com.springboot.daits;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DaitsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DaitsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/springboot/daits/DaitsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.daits;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DaitsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
