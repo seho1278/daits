a4b107555cccab742f7fa2bbd7464f5c1bf95b63
seho1278 <seho1278@naver.com>
2024-06-26 21:13:00 +0900

    .

--- src/main/java/com/springboot/daits/notice/service/PostService.java
+++ src/main/java/com/springboot/daits/notice/service/PostService.java
@@ -1,7 +1,31 @@
 package com.springboot.daits.notice.service;
 
+import com.springboot.daits.notice.repository.PostRepository;
 import org.springframework.stereotype.Service;
 
 @Service
 public class PostService {
+
+    PostRepository postRepository;
+
+    // 게시글 생성
+    public void createPost() {
+
+    }
+
+    // 게시글 조회
+    public void getPost() {
+
+    }
+
+    // 게시글 수정
+    public void updatePost() {
+
+    }
+
+    // 게시글 삭제
+    public void deletePost() {
+
+    }
+
 }
